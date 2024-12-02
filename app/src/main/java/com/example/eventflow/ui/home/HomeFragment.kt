07e6e4ec2f44a97c2392dbabdc621b5d36bdd76a
Özlem Civelek Bayraktar <cvlk.ozlem54@gmail.com>
2024-12-02 16:42:10 +0300

    Improve filter event.

--- app/src/main/java/com/example/eventflow/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eventflow/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.eventflow.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import com.example.eventflow.adapter.EventAdapter
 import com.example.eventflow.databinding.FragmentHomeBinding
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 class HomeFragment : Fragment() {
@@ -37,37 +39,25 @@ class HomeFragment : Fragment() {
         binding.recyclerView.adapter = eventAdapter
 
         setFilteredEventsForDate()
-    }
-
-    private fun setFilteredEventsForDate() {
-        val calendarView: CalendarView = binding.calendarView
-        // initial Date and Filtered
-        val initialCalendar = Calendar.getInstance()
-        initialCalendar.timeInMillis = calendarView.date
-        val initialMonth = SimpleDateFormat("MMM", Locale.ENGLISH).format(initialCalendar.time).uppercase()
-        val initialDay = String.format("%02d", initialCalendar.get(Calendar.DAY_OF_MONTH))
-        val initialDate = "$initialMonth\n$initialDay"
-
-        viewModel.filterEventsByDate(initialDate)
-
-        // CalendarView filter on date change
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-
-            val selectedMonth = SimpleDateFormat("MMM", Locale.ENGLISH).format(Calendar.getInstance().apply {
-                set(year, month, dayOfMonth)
-            }.time).uppercase()
-
-            val selectedDate = "$selectedMonth\n${String.format("%02d", dayOfMonth)}"
-            viewModel.filterEventsByDate(selectedDate)
-        }
-
         // Observe filtered Events in viewModel
         viewModel.filteredEvents.observe(viewLifecycleOwner) { filteredEvents ->
             eventAdapter.setItems(filteredEvents)
         }
     }
 
+    private fun setFilteredEventsForDate() {
+        val calendarView: CalendarView = binding.calendarView
 
+        // initial Date and Filtered
+        viewModel.filterEventsByDate(calendarView.date)
+        // CalendarView filter on date change
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val preparedDate = Calendar.getInstance().apply {
+                set(year, month, dayOfMonth)
+            }
+            viewModel.filterEventsByDate(preparedDate.timeInMillis)
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/eventflow/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/eventflow/ui/home/HomeViewModel.kt
@@ -4,23 +4,40 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.eventflow.Event
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class HomeViewModel : ViewModel() {
+
+    private val calendar by lazy {
+        Calendar.getInstance()
+    }
 
     // LiveData for filtered event list
     private val _filteredEvents = MutableLiveData<List<Event>>()
     val filteredEvents: LiveData<List<Event>> get() = _filteredEvents
 
     // Filter events by date
-    fun filterEventsByDate(date: String) {
-        val filtered = getEvents().filter { it.date == date }
+    fun filterEventsByDate(time: Long) {
+        val formattedDate = getFormattedDay(time)
+        val filtered = getEvents().filter { it.date == formattedDate }
         _filteredEvents.value = filtered
     }
 
+    private fun getFormattedDay(time: Long): String {
+        calendar.timeInMillis = time
+        val initialMonth = SimpleDateFormat("MMM", Locale.ENGLISH).format(calendar.time).uppercase()
+        val initialDay = String.format("%02d", calendar.get(Calendar.DAY_OF_MONTH))
+        return "$initialMonth\n$initialDay"
+    }
+
     private fun getEvents(): List<Event> { //Update list
-        val description = "Lorem ipsum dolor sit amet, consectetur radicalising elit, sed do usermod temper incident ut labor et do lore magna aliquot. " +
-                "Ut enum ad minim venial, quits nostrum excitation McCull och labors nisei ut aliquot ex ea commode consequent. " +
-                "Dis auto inure dolor in reprehend in voluptuary veldt ease cilium do lore eu fugit null parturition."
+        val description =
+            "Lorem ipsum dolor sit amet, consectetur radicalising elit, sed do usermod temper incident ut labor et do lore magna aliquot. " +
+                    "Ut enum ad minim venial, quits nostrum excitation McCull och labors nisei ut aliquot ex ea commode consequent. " +
+                    "Dis auto inure dolor in reprehend in voluptuary veldt ease cilium do lore eu fugit null parturition."
 
         val date = "DEC\n02"
         return listOf(
